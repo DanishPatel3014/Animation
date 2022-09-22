c6ad083d139ff37ecd0d8f5607c0da02a64fbf3c
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2022-09-22 15:50:26 -0700

    update

---
+++ includes/style.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="assets/css/layout.css">
+<link rel="stylesheet" href="assets/css/style.css">
+
+<link rel="shortcut icon" href="assets/images/fav.jpg" type="image/x-icon">
+<link rel="icon" href="assets/images/fav.jpg" type="image/x-icon">
