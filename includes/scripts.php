97b039b481560da873176d99f85738c960c992d0
DanishPatel3014 <93546025+DanishPatel3014@users.noreply.github.com>
2025-02-16 06:37:53 +0500

    updates

--- includes/scripts.php
+++
@@ -1,6 +0,0 @@
-<script src="assets/js/jquery.js"></script>
-<script src="assets/js/custom.js"></script>
-
-<script>
-	new WOW().init();
-</script>
--- includes/style.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="assets/css/layout.css">
-<link rel="stylesheet" href="assets/css/style.css">
-
-<link rel="shortcut icon" href="assets/images/fav.jpg" type="image/x-icon">
-<link rel="icon" href="assets/images/fav.jpg" type="image/x-icon">
